7ec7d9e98c7f9a4ec529a7bdc91b26e379929651
daniel <danielgadz@gmail.com>
2018-04-05 19:16:44 -0500

    nuevo mensaje de bienvenida y adicion de CircleCI

--- src/main/java/com/bap/stats/controller/Inicio.java
+++ src/main/java/com/bap/stats/controller/Inicio.java
@@ -1,15 +1,10 @@
 package com.bap.stats.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.bap.stats.service.EquipoService;
 
 @RestController
 public class Inicio {
-
-	@Autowired
-	private EquipoService equipoService;
 	
 	@GetMapping("/inicio")
 	public String bienvenido() {
