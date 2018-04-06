cccdf0ee638352d82ddccf91bf8d3eb38e448a6b
daniel <danielgadz@gmail.com>
2018-04-05 19:02:16 -0500

    nuevo mensaje de bienvenida desde el controller /inicio

--- src/main/java/com/bap/stats/controller/Inicio.java
+++ src/main/java/com/bap/stats/controller/Inicio.java
@@ -1,11 +1,8 @@
 package com.bap.stats.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.bap.stats.model.Equipo;
 import com.bap.stats.service.EquipoService;
 
 @RestController
@@ -15,12 +12,8 @@ public class Inicio {
 	private EquipoService equipoService;
 	
 	@GetMapping("/inicio")
-	public List<Equipo> bienvenido() {
-		
-		Equipo equipo = new Equipo("Mi Tercer Equipo", "dany", "/tmp/dany.png");
-		equipoService.guardarEquipo(equipo);
-		return equipoService.listarEquipos();
-		
+	public String bienvenido() {
+		return "Bienvenido a la API STATS v1";			
 	}
 
 }
