cdf86332e080444f8d599b6a238e6e4d4e86d2a7
Daniel García <danielgadz@gmail.com>
2018-04-17 17:12:20 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
-insert into equipos (id_equipo, logo, nombre) values (nextval('seq_equipo'), '/TMP/LOGO.PNG', 'MI PRIMER EQUIPO')
-insert into equipos (id_equipo, logo, nombre) values (nextval('seq_equipo'), '/TMP/LOGO2.PNG', 'MI SEGUNDO EQUIPO')
+insert into equipos (id_equipo, logo, nombre) values (nextval('seq_equipo'), 'club-img.png', 'Hobbits')
+insert into equipos (id_equipo, logo, nombre) values (nextval('seq_equipo'), 'club-img2.png', 'Control Intenso')
+insert into equipos (id_equipo, logo, nombre) values (nextval('seq_equipo'), 'club-img3.png', 'Mc Friends')
+insert into equipos (id_equipo, logo, nombre) values (nextval('seq_equipo'), 'club-img4.png', 'Rollbacks')
 
 insert into jugadores (id_jugador,nombre, correo, numero,  es_capitan,id_equipo) values (nextval('seq_jugador'), 'Juan Perez', 'juan.perez@banorte.com',8,true, 1)
 insert into jugadores (id_jugador,nombre, correo, numero,  es_capitan,id_equipo) values (nextval('seq_jugador'), 'Jose Mendez','juan.perez@banorte.com',10,true, 2)
